d43c7fa258e4488388c1476b9206d146a886cb1e
zhaoyy <16621097346@163.com>
2022-08-23 16:40:41 +0800

    feat: 添加环境变量配置.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <router-view></router-view>
 
+    <div class="mt-10 mb-10">环境变量：{{ ENV }}</div>
     <div class="mt-12 ml-5">
       <template v-for="(item, index) in routes">
         <el-button
@@ -20,12 +21,18 @@
 <script setup lang="ts">
 import { routes } from "./router";
 import { useRouter, useRoute } from "vue-router";
+import { computed } from "vue";
 const router = useRouter();
 const route = useRoute();
 
 const jump = (item: string) => {
-  router.push(item)
+  router.push(item);
 };
+
+const ENV = computed(() => {
+  return import.meta.env;
+});
+console.log(import.meta.env.VITE_BASEURL);
 </script>
 
 <style scoped></style>
